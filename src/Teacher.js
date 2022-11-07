3712ed46046d1dbfb7fc2400c675438bc2cb383e
BlindRhythm <64942828+BlindRhythm@users.noreply.github.com>
2022-11-06 23:23:06 -0500

    Update Teacher.js

--- src/Teacher.js
+++ src/Teacher.js
@@ -9,33 +9,32 @@ function QuestionToHTMLStrTeacher(assessment, question, index)
 function AssessmentToHTMLStrTeacher(assessment)
 {
     let htmlStr = "<p>" + assessment.GetName() + "</p>";
+    
     /*
     htmlStr += "<table><tr><th>Weight</th><th>Class Average</th></tr>" + "<tr><td>" + assessment.GetWeight() + "</td>" 
         + "<td>" + assessment.GetAverage() + "</td></tr>";
     */
 
-    htmlStr += "<p>Distribution</p><ul>";
+    htmlStr += "<p>Distribution</p>";
     let distribution = assessment.GetDistribution();
     distribution.forEach(function(numStudents, grade)
     {
-        htmlStr += "<li>Grade: " + grade + " Number of Students: " + numStudents + "</li>";
+        htmlStr += "<table><tr><td><b>Grade: </b>" + grade + "</td>" + "<tr><td>Number of Students: " + numStudents + "</td></tr></table>";
     });
 
-    htmlStr += "</ul><p>Marks:</p><ul>";
+    htmlStr += "<p>Marks</p>";
     let totals = assessment.GetTotals();
     totals.forEach(function (total, id)
     {
-        htmlStr += "<li>ID: " + id + " Total: " + total + "</li>";
+        htmlStr += "<table><tr><th>ID: " + id + "</th>" + "<tr><th> Total: " + total + "</th></tr></table>";
     });
-    htmlStr += "</ul><p>Questions:</p><ul>";
+    htmlStr += "<p>Questions</p>";
 
     let questions = assessment.GetQuestions();
     for (let i = 0; i < questions.length; i++)
     {
         htmlStr += "<li>" + QuestionToHTMLStrTeacher(assessment, questions[i], i) + "</li>";
     }
-    
-    htmlStr += "</ul>";
     return htmlStr;
 }
 
@@ -64,6 +63,8 @@ function GenerateTeacherBody(user)
 {
     let body = "<body>";
     let assessments = user.GetAssessmentsTeacher();
+    
+    body+=GenerateHeader();
     // overview
     body += GenerateOverview(assessments);
 
@@ -76,13 +77,30 @@ function GenerateTeacherBody(user)
     return body;
 }
 
+function GenerateStyle()
+{
+    return "<style> table, th, td {border:1px solid black;} </style>"
+}
+
+function GenerateHeader()
+{
+    return "<h1 style=\"position: relative; padding: 0.1%; bottom: 82%; top: 0%; left: 0%; right: 0%; text-align: center; background: #912338; color: white;\">Concordia University</h1>"
+    //<p style = \"position: fixed; padding: 0.1%; bottom: 82%; top: 0%; left: 0%; right: 0%; text-align: center; background: #912338; color: white;\">Marks and Grades assessment for SOEN 287 for Fall 2022</p>"
+}
+function GenerateFooter()
+{
+    return "<h6 style=\"position: relative; padding: 1%; bottom: 0%; top: 92%; left: 0%; right: 0%; text-align: center; background: #912338; color: white;\">Website made by Hao Mei, Jamil Hanachian, James Teasdale, Alex Ye, Catherine Pham & Nikita Ciobanu</h6>"
+}
+
 function LoadTeacherHomePage(user)
 {
     console.log("loading /teacher/home/" + user.GetID());
     let teacherPage = util.GenerateHTMLHeader();
     teacherPage += GenerateTeacherPageHead();
+    teacherPage += GenerateStyle();
     teacherPage += GenerateTeacherBody(user);
-    teacherPage += util.GenerateHTMLFooter();
+    teacherPage += GenerateFooter();
+    //teacherPage += util.GenerateHTMLFooter();
     return teacherPage; 
 }
 
