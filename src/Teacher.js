a9ddeeeba7c57b41959f8d8395ec124e539b69e4
hao120899 <84607659+hao120899@users.noreply.github.com>
2022-11-09 13:13:17 -0500

    fixed the two requested changes

--- src/Teacher.js
+++ src/Teacher.js
@@ -6,6 +6,57 @@ function QuestionToHTMLStrTeacher(assessment, question, index)
     return "On " + question.GetMaxGrade() + "\tAverage: " + assessment.GetQuestionAverage(index);
 }
 
+function AssessmentToHTMLStrTeacherGrade()
+{
+    return `
+    <div ><h2>SOEN 287 Section Q</h2> </div>
+    <div><h3>Add assessment for a student</h3></div>
+    <div class="mainBox">
+        <div class="text02Box">		
+            <form action="#">
+                 <label for="lang">Assessment:</label>
+                 <select name="languages" id="lang">
+                    <option value="Quiz">Quiz</option>
+                    <option value="Reflection Essay">Reflection Essay</option>
+                    <option value="Midterm Exam">Midterm Exam</option>
+                    <option value="Final Exam">Final Exam</option>
+                 </select>
+            </form>
+            <br>
+            <span class="datum" >Weight:</span>
+            <input id="in2" type="text"/><span> % </span>
+            <br>
+            <br>
+            <span class="datum">Number of questions:</span>
+            <input  id = "in3" type="number"/>
+            <br>
+            <br>
+            <button class="dropbtn">Create Assessment</button>
+            <br>
+            <table>
+                <tr>
+                    <th>Max Value For Each Question</th>
+                    <td> <input type="number"> </td>
+                    <td> <input type="number"> </td>
+                    <td> <input type="number"> </td>
+                </tr>
+                <tr>
+                    <td>645258</td>
+                    <td> <input type="number"> </td>
+                    <td> <input type="number"> </td>
+                    <td> <input type="number"> </td>
+                </tr>
+                <tr>
+                    <td>432483</td>
+                    <td> <input type="number"> </td>
+                    <td> <input type="number"> </td>
+                    <td> <input type="number"> </td>
+                </tr>
+            </table>
+           
+        </div>
+    </div> `
+}
 function AssessmentToHTMLStrTeacher(assessment)
 {
     let htmlStr = "<p>" + assessment.GetName() + "</p>";
@@ -70,6 +121,7 @@ function LoadTeacherHomePage(user)
     console.log("loading /teacher/home/" + user.GetID());
     let teacherPage = util.GenerateHTMLHeader();
     teacherPage += GenerateTeacherPageHead();
+    teacherPage += AssessmentToHTMLStrTeacherGrade();
     teacherPage += GenerateTeacherBody(user);
     teacherPage += util.GenerateHTMLFooter();
     return teacherPage; 
