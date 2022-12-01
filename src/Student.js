da411c1f0b2d2da202c0788af9e16c9fffe929a7
Nikytus <115109561+Nikytus@users.noreply.github.com>
2022-12-01 12:14:34 -0500

    Update Student.js

--- src/Student.js
+++ src/Student.js
@@ -7,14 +7,28 @@ function QuestionToHTMLStrStudent(assessment, index)
     return assessment.GetQuestionGrade(index) + "/" + assessment.GetQuestionMaxGrade(index);
 }
 
+function GenerateHeader()
+{
+    return "<h1 style=\"position: relative; width:100% ; padding: 0.2%; bottom: 85%; top: -30px; left: -10px; right: 0; " +
+        "text-align: center;font-size:40px; background: #912338; color: white;\">Concordia University</h1>";
+        
+}
+function GenerateFooter()
+{
+    return "<h5 style=\"position: fixed; padding: 1%; bottom: -22px; top: 95%; left: 0%; " + 
+        "right: 0%; text-align: center; background: #912338; color: white;\"> " + 
+        "Website made by Hao Mei, Jamil Hanachian, James Teasdale, Alex Ye, Catherine Pham " + 
+        "& Nikita Ciobanu</h6>";
+}
+
 function AssessmentToHTMLStrStudent(assessment)
 {
-    var htmlStr = "<p>" + assessment.GetName() +  "</p>"; 
-    htmlStr += "<p>Mark:&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;" + assessment.GetGrade() + "/" + assessment.GetMaxGrade() + "</p>";
-    htmlStr +=  "<p>Weight: &emsp;&emsp;&emsp;&emsp;&emsp;" + ((assessment.GetWeightedGrade()*100)/100).toFixed(2) + "/" + assessment.GetWeight() + "</p>";
+    var htmlStr = "<h2><b><u>"  + assessment.GetName() +  "</u></h2>"; 
+    htmlStr += "<p>Mark:&emsp;&emsp;&emsp;&emsp;" + assessment.GetGrade() + "/" + assessment.GetMaxGrade() + "</p>";
+    htmlStr +=  "<p>Weight: &emsp;&emsp;&emsp;" + ((assessment.GetWeightedGrade()*100)/100).toFixed(2) + "/" + assessment.GetWeight() + "</p>";
     for (let i = 0; i < assessment.GetNumQuestions(); i++)
     {
-        htmlStr += "&emsp;Question " + (i+1) + "&emsp;&emsp;&emsp;" + QuestionToHTMLStrStudent(assessment, i) + "<br>";
+        htmlStr +="<table><tr><td>"+ "&emsp;Question " + (i+1) + "&emsp;&emsp;&emsp;" + QuestionToHTMLStrStudent(assessment, i) + "</td></tr></table>";
     }
     //insert stats here
     {
@@ -25,24 +39,33 @@ function AssessmentToHTMLStrStudent(assessment)
 
     return htmlStr;
 }
-
+function GenerateGradeSystem(){
+    var GPA ="<table><caption><h3 style='color: #912338'>Grade System</h3></caption><tr><th>Letter Grade</th><th>GPA </th></tr><tr><td>A+</td><td>4.3</td></tr><tr><td>A</td><td>4.0</td> </tr><tr><td>A-</td><td>3.7</td> </tr><tr><td>B+</td><td>3.3</td> </tr><tr><td>B</td><td>3.0</td> </tr><tr><td>B-</td><td>2.7</td> </tr><tr><td>C+</td><td>2.3</td> </tr><tr><td>C</td><td>2.0</td> </tr><tr><td>C-</td><td>1.7</td> </tr><tr><td>D+</td><td>1.3</td> </tr> <tr><td>D</td><td>1.0</td> </tr><tr><td>D-</td><td>0.7</td> </tr><tr><td>F</td><td>0</td> </tr></table>"
+    return GPA
+}
 function GenerateStudentPageHead()
 {
-    return "<head></head>";
+    return "<head>"+ GenerateStyle()+"</head>";
 }
-
+function GenerateStyle()
+{
+    return "<style> table, th, td {border:1px solid black;border-radius: 10px;width:200px;font-weight: bold;} u{color: #912338}  </style>"
+}
 function GenerateStudentBody(user)
 {
     let body = "<body>";
-
+    body += GenerateHeader();
     let assessments = user.GetAssessmentsStudent();
     assessments.forEach(function (assessment)
     {
         body += AssessmentToHTMLStrStudent(assessment);
     });
-    
+    body += GenerateFooter();
     body += "<p>Total: &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;" + user.GetFinalGrade().toFixed(2) + "/100</p>";
-    body += "<p>Letter Grade:&emsp;&emsp;&emsp;" + user.GetLetterGrade() + "</p>"; 
+    body +=  "<table><tr><td> Student ID: &emsp;" +user.GetID();
+    body += "<table><tr><td> Letter Grade: " + user.GetLetterGrade() ; 
+    body += "</table>";
+    body += GenerateGradeSystem();
     body += "</body>";
 
     return body;
